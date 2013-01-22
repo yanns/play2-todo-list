3b71456575321065875d149ca6e89c9d95e652b7
Yann Simon <yann.simon.fr@gmail.com>
2013-01-22 08:33:21 +0100

    implement login (http://localhost:9000/login)

---
+++ app/controllers/Credential.java
@@ -0,0 +1,45 @@
+package controllers;
+
+
+import models.User;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.login;
+
+public class Credential extends Controller {
+
+    public static class Login {
+
+        public String email;
+        public String password;
+
+        public String validate() {
+            if (User.authenticate(email, password) == null) {
+                return "Invalid user or password";
+            }
+            return null;
+        }}
+
+
+    public static Result login() {
+        return ok(
+            login.render(form(Login.class))
+        );
+    }
+
+    public static Result authenticate() {
+        Form<Login> loginForm = form(Login.class).bindFromRequest();
+        if (loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+        } else {
+            session().clear();
+            session("email", loginForm.get().email);
+            return redirect(
+                routes.Application.index()
+            );
+        }
+    }
+
+
+}
---
+++ test/controllers/CredentialTest.java
@@ -0,0 +1,56 @@
+package controllers;
+
+import com.avaje.ebean.Ebean;
+import com.google.common.collect.ImmutableMap;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import play.libs.Yaml;
+import play.mvc.*;
+import play.test.*;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static play.test.Helpers.*;
+
+public class CredentialTest {
+
+    private FakeApplication fakeApplication;
+
+    @Before
+    public void setUp() {
+        fakeApplication = fakeApplication(inMemoryDatabase());
+        start(fakeApplication);
+        Ebean.save((List) Yaml.load("test-data.yml"));
+    }
+
+    @Test
+    public void authenticateSuccess() {
+        Result result = callAction(
+                controllers.routes.ref.Credential.authenticate(),
+                fakeRequest().withFormUrlEncodedBody(ImmutableMap.of(
+                        "email", "bob@example.com",
+                        "password", "secret"))
+        );
+        assertEquals(303, status(result));
+        assertEquals("bob@example.com", session(result).get("email"));
+    }
+
+    @Test
+    public void authenticateFailure() {
+        Result result = callAction(
+                controllers.routes.ref.Credential.authenticate(),
+                fakeRequest().withFormUrlEncodedBody(ImmutableMap.of(
+                        "email", "bob@example.com",
+                        "password", "badpassword"))
+        );
+        assertEquals(400, status(result));
+        assertNull(session(result).get("email"));
+    }
+
+    @After
+    public void tearDown() {
+        stop(fakeApplication);
+    }
+}
